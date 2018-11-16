ba1a9bb10743f3d2fc1f40730f2c3bcbc3fa95e4
Rémy Taymans <remytaymans@gmail.com>
2018-11-16 12:09:02 +0100

    [REF] website_sale_category_megamenu: Use customize menu
    Split functionalities in different templates that can be enabled or
    disabled in the Customize menu in the website.
    
    Create a new division in the html where mega menu and other
    functionalities are placed.
    
    Facilitate the extension of this module by having specific templates to
    insert functionalities in the parent view. So that new component can be
    moved easier by another module.
    
    Replace the usage of `@class` by `hasclass`.
    
    This module is like this more generic.

--- website_sale_category_megamenu/__manifest__.py
+++ website_sale_category_megamenu/__manifest__.py
@@ -1,6 +1,7 @@
 # © 2014-2015 Antiun Ingenieria, SL (Madrid, Spain, http://www.antiun.com)
 #             Antonio Espinosa <antonioea@antiun.com>
 #             Daniel Gómez-Zurita <danielgz@antiun.com>
+# Copyright 2018 Coop IT Easy SCRLfs
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
@@ -9,7 +10,8 @@
     "version": "11.0.1.0.1",
     "category": "Themes/Miscellaneous",
     "website": "https://github.com/OCA/e-commerce/",
-    "author": "Tecnativa, "
+    "author": "Coop IT Easy, "
+              "Tecnativa, "
               "Antiun Ingeniería S.L., "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
