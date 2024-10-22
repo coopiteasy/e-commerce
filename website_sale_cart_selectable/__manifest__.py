077b2602e7292b09f1582a2770d07f5dc71f0d43
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2024-10-22 14:49:33 +0200

    [MIG] website_sale_cart_selectable: Migration to 14.0
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

--- website_sale_cart_selectable/__manifest__.py
+++ website_sale_cart_selectable/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Website Sale Cart Selectable",
     "summary": "Enables to control button Add to cart display per product",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Website",
     "website": "https://github.com/OCA/e-commerce",
     "author": "OpenSynergy Indonesia, Tecnativa,"
